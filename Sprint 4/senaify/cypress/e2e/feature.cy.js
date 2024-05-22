7a07914c3c12499a21d799956c947c614ccb8059
Miguel Lamarca Moreira <50866670890@info.local>
2024-05-22 15:09:33 -0300

    fim da parte de testes

--- Sprint 4/senaify/cypress/e2e/feature.cy.js
+++ Sprint 4/senaify/cypress/e2e/feature.cy.js
@@ -7,34 +7,52 @@ describe('template spec', () => {
   it('Visualizar playlists e executar uma música', () => {
     cy.get("[aria-label='title-head']").should("contain", "Good morning")
     cy.get("[aria-label='playlist-item']").should("have.length.greaterThan", 0)
+    cy.wait(1500)
     cy.get("[aria-label='playlist-item']").first().click()
     cy.get("[aria-label='music-item']").should("have.length.greaterThan", 0)
+    cy.wait(1500)
     cy.get("[aria-label='music-item']").first().click()
   })
 
   it("Navegar entre playlists e executar outra música", () => {
-    cy.wait(5000)
+    cy.wait(2000)
+    cy.scrollTo("top")
     cy.get("[aria-label='back-button']").click()
+    cy.scrollTo("top")
+    cy.wait(1500)
     cy.get("[aria-label='playlist-item']").eq(1).click()
+    cy.wait(1500)
     cy.get("[aria-label='music-item']").should("have.length.greaterThan", 0)
     cy.get("[aria-label='music-item']").first().click()
+    
   })
 
   it("Procurar e favoritar uma música", () => {
-    cy.wait(5000)
+    cy.wait(2000)
     cy.get("[aria-label='back-button']").click()
+    cy.scrollTo("top")
+    cy.wait(1500)
     cy.get("[href='/Search']").click()
-    cy.get("[data-testid='campoBusca']").type("Quero Ser Feliz Também");
+    cy.scrollTo("top")
+    cy.wait(1500)
+    cy.get("[data-testid='campoBusca']").type("Aquariano Nato");
+    cy.wait(1500)
     cy.get("[aria-label='music-item']").should("have.length.greaterThan", 0)
     musicaPreferida = cy.get("[aria-label='music-item']").first()
     musicaPreferida.click()
+    cy.wait(8000)
+    musicaPreferida.click()
+    cy.wait(1500)
     cy.get(musicaPreferida).get("[data-testid='icon-button']").first().click()
   })
 
   it('Verificar música favoritada na tela de Favoritos', () => {
-    cy.wait(5000)
+    cy.wait(2000)
     cy.get("[href='/Favorites']").click()
+    cy.scrollTo("top")
+    cy.wait(1500)
     cy.get("[aria-label='music-item']").should("have.length.greaterThan", 0)
-    musicaPreferida.click()
+    cy.wait(1500)
+    cy.get("[aria-label='music-item']").contains("Aquariano Nato").click({force: true})
   })
 })
